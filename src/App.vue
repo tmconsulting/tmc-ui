27551de4d3a74e86d23d4b25746b62ffcf42f47c
mgorunuch <mgorunuch.igor@gmail.com>
2018-12-16 05:44:03 +0200

    initial library commit

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <banner><h1>Привет, TMC-UI!</h1></banner>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+  import {Component, Vue} from 'vue-property-decorator';
+  import Banner from './components/banner.vue';
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class App extends Vue {}
+  @Component({
+    components: {
+      Banner,
+    },
+  })
+  export default class App extends Vue {
+  }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body, html {
+    margin: 0;
+    padding: 20px;
+    box-sizing: border-box;
+  }
 </style>
---
+++ src/components/banner.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="banner">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+  import {Component, Vue} from 'vue-property-decorator';
+
+  @Component
+  export default class Banner extends Vue {
+  }
+</script>
+
+<style lang="scss">
+  .banner {
+    width: 100%;
+    font-family: 'sans-serif';
+    padding: 20px;
+    border: solid 2px;
+    box-sizing: border-box;
+    text-align: center;
+  }
+</style>
